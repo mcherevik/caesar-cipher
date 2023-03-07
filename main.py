3b3aa3d0b6822937d514da8487e90d2ffc8f289f
mcherevik <mcherevik1@gmail.com>
2023-03-07 18:32:35 +0200

    added Russian language

--- main.py
+++ main.py
@@ -39,6 +39,32 @@ def english_decipher(text, shift):
     return ciphered_text
 
 
+def russian_cipher(text, shift):
+    ciphered_text = ''
+    for s in text:
+        if s.isalpha():
+            if s.islower() and ord(s) + shift > 1103 or s.isupper() and ord(s) + shift > 1071:
+                ciphered_text += chr(ord(s) + shift - 32)
+            else:
+                ciphered_text += chr(ord(s) + shift)
+        else:
+            ciphered_text += s
+    return ciphered_text
+
+
+def russian_decipher(text, shift):
+    ciphered_text = ''
+    for s in text:
+        if s.isalpha():
+            if s.islower() and ord(s) - shift < 1072 or s.isupper() and ord(s) - shift < 1040:
+                ciphered_text += chr(ord(s) - shift + 32)
+            else:
+                ciphered_text += chr(ord(s) - shift)
+        else:
+            ciphered_text += s
+    return ciphered_text
+
+
 def choose_var():
     while True:
         answer = input()
@@ -50,6 +76,8 @@ def choose_var():
             print('We do not understand your answer. Please try again')
 
 
+print("Please choose the language: press 1 for English and 2 for Russian")
+language_choice = choose_var()
 print("Please choose the shift direction: press 1 for ciphering (a + 3 = d), 2 for deciphering (d - 3 = a)")
 direction_choice = choose_var()
 print("Please enter your text in one line")
@@ -57,7 +85,11 @@ initial_text = input()
 print("Please enter the shift size")
 user_shift = valid_number()
 
-if direction_choice:
+if direction_choice and language_choice:
     print(english_cipher(initial_text, user_shift))
-elif not direction_choice:
+elif not direction_choice and language_choice:
     print(english_decipher(initial_text, user_shift))
+elif direction_choice and not language_choice:
+    print(russian_cipher(initial_text, user_shift))
+elif not direction_choice and not language_choice:
+    print(russian_decipher(initial_text, user_shift))
